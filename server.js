31c30943e4162c946c41073e891ff5e577c046a5
agung widhiatmojo <agung.widhiatmojo@gmail.com>
2024-05-25 01:28:23 +0700

    add health check api endpoint

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 import productRoutes from './routes/productRoutes.js';
+import health from './routes/health.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 
 app.use('/auth', authRoutes);
 app.use('/products', productRoutes);
+app.use('/', health);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
